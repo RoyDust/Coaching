427adbe497c059fe2ff4398e88982914cfc858ad
RoyDust <3214026782@qq.com>
2023-10-12 19:15:57 +0800

    fix: route fix

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 export async function GET(req: Request) {
   try {
     const response = await axios.post(
-      `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY}&response=${token}`,
+      `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY}`,
       {},
       {
         headers: {
